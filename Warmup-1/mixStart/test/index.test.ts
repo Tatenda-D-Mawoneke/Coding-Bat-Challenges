d53814c0bdf060c422443a675cd491439af6536c
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-03 19:05:43 +0200

    re-arranged folder structure

---
+++ Warmup-1/mixStart/test/index.test.ts
@@ -0,0 +1,13 @@
+/*
+Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false
+*/
+
+describe("mixStart", () => {
+  it("should return true when string is mix snacks", () => {});
+  it("should", () => {});
+  it("should", () => {});
+});
