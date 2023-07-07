ddbe399cbe73b802c6acf2f1dad1221b5d71ac44
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-07 12:45:04 +0200

    implement slightly 'cleaner' code for posNeg

--- Warmup-1/posNeg/src/index.ts
+++ Warmup-1/posNeg/src/index.ts
@@ -1,24 +1,20 @@
+// Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+// posNeg(1, -1, false) → true
+// posNeg(-1, 1, false) → true
+// posNeg(-4, -5, true) → true
+
 export function posNeg(num1: number, num2: number, negative: boolean): boolean {
+  // attempt 2:
   let myResult: boolean = false;
-  const myBoolean1 = Boolean(num1);
-  const myBoolean2 = Boolean(num2);
-  switch (negative) {
-    case false:
-      if (
-        (myBoolean1 && !myBoolean2 == false) ||
-        (!myBoolean1 == false && myBoolean2)
-      ) {
-        myResult = true;
-      }
-      break;
-    case true:
-      if (!myBoolean1 == false && !myBoolean2 == false) {
-        myResult = true;
-      }
-      break;
-    default:
-      console.log("There has to be an easier way to do this :'s");
-      break;
+  const myNum1 = Math.sign(num1);
+  const myNum2 = Math.sign(num2);
+
+  if (myNum1 * myNum2 == -1 && !negative) {
+    myResult = true;
+  }
+  if (myNum1 * myNum2 == 1 && negative) {
+    myResult = true;
   }
   return myResult;
 }
