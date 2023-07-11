13d18c4fc7343913deb56fb446c3676e7d360e80
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-11 22:28:24 +0200

    add frontBack

---
+++ Warmup-1/frontBack/test/index.test.ts
@@ -0,0 +1,23 @@
+import { assert } from "chai";
+import { frontBack } from "../src";
+
+// Given a string, return a new string where the first and last chars have been exchanged.
+
+// frontBack("code") → "eodc"
+// frontBack("a") → "a"
+// frontBack("ab") → "ba"
+
+describe("frontBack", () => {
+  it("should return 'eodc' when passed 'code", () => {
+    const result = frontBack("code");
+    assert.equal(result, "eodc");
+  });
+  it("should return 'a' when passed 'a", () => {
+    const result = frontBack("a");
+    assert.equal(result, "a");
+  });
+  it("should return 'ab' when passed 'ab", () => {
+    const result = frontBack("ab");
+    assert.equal(result, "ba");
+  });
+});
