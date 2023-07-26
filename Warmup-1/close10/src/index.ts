5c9c78914e84bef2747ab04ab06442ade35e8ca9
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-26 19:42:48 +0200

    remove unnecesary commented out code

--- Warmup-1/close10/src/index.ts
+++ Warmup-1/close10/src/index.ts
@@ -1,13 +1,4 @@
 export function close10(input1: number, input2: number): number {
-  // const input1Modulo10 = Math.abs(input1 % 10);
-  // const input2Modulo10 = Math.abs(input2 % 10);
-  // if (input1Modulo10 < input2Modulo10) {
-  //   return input2;
-  // } else if (input2Modulo10 < input1Modulo10) {
-  //   return input1;
-  // }
-  // return 0;
-
   const input1Remainder = Math.abs(10 - input1);
   const input2Remainder = Math.abs(10 - input2);
   if (input1Remainder < input2Remainder) {
