4a1d9b5462cae13cf8735ae9dd4a4c12d3898ee5
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-26 20:56:52 +0200

    simplify code

--- Warmup-1/startOz/src/index.ts
+++ Warmup-1/startOz/src/index.ts
@@ -1,11 +1,24 @@
 export function startOz(givenInput: string): string {
-  const myStringArray = [...givenInput];
-  if (myStringArray[0] == "o" && myStringArray[1] == "z") {
-    return "oz";
-  } else if (myStringArray[0] == "o" && myStringArray[1] != "z") {
-    return "o";
-  } else if (myStringArray[0] != "o" && myStringArray[1] == "z") {
-    return "z";
+  // const myStringArray = [...givenInput];
+  // if (myStringArray[0] == "o" && myStringArray[1] == "z") {
+  //   return "oz";
+  // } else if (myStringArray[0] == "o" && myStringArray[1] != "z") {
+  //   return "o";
+  // } else if (myStringArray[0] != "o" && myStringArray[1] == "z") {
+  //   return "z";
+  // }
+  // return "you failed!";
+
+  const firstCharacter = givenInput[0];
+  const secondCharacter = givenInput[1];
+  let myString = "";
+
+  if (firstCharacter == "o") {
+    myString = "o";
   }
-  return "you failed!";
+  if (secondCharacter == "z") {
+    myString = myString + "z";
+  }
+
+  return myString;
 }
