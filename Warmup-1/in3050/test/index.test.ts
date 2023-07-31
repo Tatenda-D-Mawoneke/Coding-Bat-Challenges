4b65e596334b5fd0cbcd659c9c91d04f88addd8a
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-07-31 19:07:56 +0200

    add in3050

--- Warmup-1/in3050/test/index.test.ts
+++ Warmup-1/in3050/test/index.test.ts
@@ -1,9 +1,23 @@
 // Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
 
+import { assert } from "chai";
+import { in3050 } from "../src";
+
 // in3050(30, 31) → true
 // in3050(30, 41) → false
 // in3050(40, 50) → true
 
-describe("in3050", =>{
-    it("should ")
+describe("in3050", ()=>{
+    it("should return true if passed 30, 31", ()=>{
+        const result = in3050(30,31);
+        assert.equal(result, true)
+    })
+    it("should return false if passed 30,41", ()=>{
+        const result = in3050(30,41);
+        assert.equal(result, false)
+    })
+    it("should return true if passed 40,50", ()=>{
+        const result = in3050(40,50);
+        assert.equal(result, true)
+    })
 })
