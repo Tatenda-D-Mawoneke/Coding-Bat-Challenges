9b32583fcaf7d497c2e3ead02dc68a9633c2b801
Tatenda-D-Mawoneke <91260404+Tatenda-D-Mawoneke@users.noreply.github.com>
2023-08-02 20:30:05 +0200

    add loneTeen (#21)
    * add loneTeen
    
    * refactor code
    
    * added new test, simplified code
    
    * fix a defect in test

---
+++ Warmup-1/loneTeen/test/index.test.ts
@@ -0,0 +1,27 @@
+// We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+import { assert } from "chai";
+import { loneTeen } from "../src";
+
+// loneTeen(13, 99) → true
+// loneTeen(21, 19) → true
+// loneTeen(13, 13) → false
+
+describe("loneTeen", () => {
+  it("should return true when the given value (13, 99) is within the range of 13..19 inclusive", () => {
+    const result = loneTeen(13, 99);
+    assert.equal(result, true);
+  });
+  it("should return true when the given value (21, 19) is within the range of 13..19 inclusive", () => {
+    const result = loneTeen(21, 19);
+    assert.equal(result, true);
+  });
+  it("should return false when the given value (13, 13) as both are teens", () => {
+    const result = loneTeen(13, 13);
+    assert.equal(result, false);
+  });
+  it("should return false when the given value (12, 12) as both are not teens", () => {
+    const result = loneTeen(12, 12);
+    assert.equal(result, false);
+  });
+});
