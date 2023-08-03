cc7b30379a4781d50b6a8c3a91262b060020eb91
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-08-03 21:47:02 +0200

    removed commented out code

--- Warmup-1/startOz/src/index.ts
+++ Warmup-1/startOz/src/index.ts
@@ -1,14 +1,4 @@
 export function startOz(givenInput: string): string {
-  // const myStringArray = [...givenInput];
-  // if (myStringArray[0] == "o" && myStringArray[1] == "z") {
-  //   return "oz";
-  // } else if (myStringArray[0] == "o" && myStringArray[1] != "z") {
-  //   return "o";
-  // } else if (myStringArray[0] != "o" && myStringArray[1] == "z") {
-  //   return "z";
-  // }
-  // return "you failed!";
-
   const firstCharacter = givenInput[0];
   const secondCharacter = givenInput[1];
   let myString = "";
