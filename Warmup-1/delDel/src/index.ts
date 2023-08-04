7b1086c2363419cbe31710427272802f6ba17d9d
Tatenda Mawoneke <tdmawoneke@gmail.com>
2023-08-04 13:57:22 +0200

    simplified code

--- Warmup-1/delDel/src/index.ts
+++ Warmup-1/delDel/src/index.ts
@@ -1,8 +1,4 @@
 export function delDel(myString: string): string {
   const mySubString = myString[1] + myString[2] + myString[3];
-
-  if (mySubString == "del") {
-    return myString.replace("del", "");
-  }
-  return myString;
+  return mySubString == "del" ? myString.replace("del", "") : myString;
 }
