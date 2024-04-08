260482a67cee0182b04533b250db24ef4a8cf9e0
Tatenda-D-Mawoneke <91260404+Tatenda-D-Mawoneke@users.noreply.github.com>
2024-04-08 17:46:24 +0200

    add frontTimes (#36)
    * add frontTimes
    
    * Refactor code code and improve stability. Also add more tests

---
+++ Warmup-2/frontTimes/test/index.test.ts
@@ -0,0 +1,31 @@
+// Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+import { assert } from "chai";
+import { frontTimes } from "../src";
+
+// frontTimes("Chocolate", 2) → "ChoCho"
+// frontTimes("Chocolate", 3) → "ChoChoCho"
+// frontTimes("Abc", 3) → "AbcAbcAbc"
+
+describe("frontTimes", () => {
+  it("should return 'ChoCho' when passed 'Chocolate' and '2'", () => {
+    const result = frontTimes("Chocolate", 2);
+    assert.equal(result, "ChoCho");
+  });
+  it("should return 'ChoChoCho' when passed 'Chocolate' and '3'", () => {
+    const result = frontTimes("Chocolate", 3);
+    assert.equal(result, "ChoChoCho");
+  });
+  it("should return 'AbcAbcAbc' when passed 'Abc' and '3'", () => {
+    const result = frontTimes("Abc", 3);
+    assert.equal(result, "AbcAbcAbc");
+  });
+  it("should return 'AB' when passed 'Chocolate' and '2'", () => {
+    const result = frontTimes("AB", 2);
+    assert.equal(result, "ABAB");
+  });
+  it("should return 'Test' when passed 'Abc' and '1'", () => {
+    const result = frontTimes("Test", 1);
+    assert.equal(result, "Tes");
+  });
+});
