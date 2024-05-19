9670f86efce459f5bc49eaeb6ddd829a90a57421
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-05-19 20:06:07 +0200

    add commonEnd

--- Array-1/commonEnd/src/index.ts
+++ Array-1/commonEnd/src/index.ts
@@ -1,3 +1,3 @@
-export function commonEnd (input: string){
-    return input
+export function commonEnd (a: number[], b: number[]): boolean{
+    return a[0] == b[0] || a.slice(-1)[0] == b.slice(-1)[0]
 }
--- Array-1/commonEnd/test/index.test.ts
+++ Array-1/commonEnd/test/index.test.ts
@@ -1,15 +1,23 @@
-// Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
-
 import { assert } from "chai";
 import { commonEnd } from "../src/index.js";
+
+// Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
 
 // commonEnd([1, 2, 3], [7, 3]) → true
 // commonEnd([1, 2, 3], [7, 3, 2]) → false
 // commonEnd([1, 2, 3], [1, 3]) → true
 
 describe("commonEnd", () => {
-	it("Should true when given two arrays of ints and they have the same first element or same last element", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+	it("Should true when given [1, 2, 3], [7, 3]", () => {
+		const result = commonEnd([1, 2, 3], [7, 3]);
+		assert.equal(result, true);
+	});
+	it("Should false when given [1, 2, 3], [7, 3, 2]", () => {
+		const result = commonEnd([1, 2, 3], [7, 3, 2]);
+		assert.equal(result, false);
+	});
+	it("Should true when given [1, 2, 3], [1, 3]", () => {
+		const result = commonEnd([1, 2, 3], [1, 3]);
+		assert.equal(result, true);
 	});
 });
