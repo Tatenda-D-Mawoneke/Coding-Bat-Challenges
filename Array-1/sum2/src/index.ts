1a583a4b982f4e0b153916702827c74bc11d10af
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-05-20 18:13:50 +0200

    add sum2

--- Array-1/sum2/src/index.ts
+++ Array-1/sum2/src/index.ts
@@ -1,3 +1,9 @@
-export function commonEnd(input: string) {
-	return input;
+export function sum2(input: number[]): number {
+	if(input.length == 0) return 0
+
+	const additive1 = input[0] ? input[0] : NaN
+	const additive2 = input[1] ? input[1] : NaN
+
+	return additive1 + additive2
+
 }
--- Array-1/sum2/test/index.test.ts
+++ Array-1/sum2/test/index.test.ts
@@ -1,15 +1,23 @@
-// Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
-
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { sum2 } from "../src/index.js";
 
-// commonEnd([1, 2, 3], [7, 3]) → true
-// commonEnd([1, 2, 3], [7, 3, 2]) → false
-// commonEnd([1, 2, 3], [1, 3]) → true
+// Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+// sum2([1, 2, 3]) → 3
+// sum2([1, 1]) → 2
+// sum2([1, 1, 1, 1]) → 2
 
 describe("Template", () => {
-	it("Should true when given two arrays of ints and they have the same first element or same last element", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+	it("Should return 3 when given [1, 2, 3]", () => {
+		const result = sum2([1, 2, 3]);
+		assert.equal(result, 3);
+	});
+	it("Should return 2 when given [1, 1]", () => {
+		const result = sum2([1, 1]);
+		assert.equal(result, 2);
+	});
+	it("Should return 2 when given [1, 1, 1, 1]", () => {
+		const result = sum2([1, 1, 1, 1]);
+		assert.equal(result, 2);
 	});
 });
