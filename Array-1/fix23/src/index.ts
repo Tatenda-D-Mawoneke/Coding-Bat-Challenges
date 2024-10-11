1ad62c66e3d41e5122c3ab6d2371debc294ee690
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-10-11 08:58:20 +0200

    add fix23

--- Array-1/fix23/src/index.ts
+++ Array-1/fix23/src/index.ts
@@ -1,3 +1,11 @@
-export function commonEnd(input: number): number {
-	return
+export function fix23(input: number[]): number[] {
+	if (input[0] == 2 && input[1] == 3) {
+		input[1] = 0;
+		return input;
+	}
+	if (input[1] == 2 && input[2] == 3) {
+		input[2] = 0;
+		return input;
+	}
+	return input
 }
--- Array-1/fix23/test/index.test.ts
+++ Array-1/fix23/test/index.test.ts
@@ -1,13 +1,26 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { fix23 } from "../src/index.js";
 
 /*
+Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
 
+
+fix23([1, 2, 3]) → [1, 2, 0]
+fix23([2, 3, 5]) → [2, 0, 5]
+fix23([1, 2, 1]) → [1, 2, 1]
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("fix23", () => {
+	it("Should return [1, 2, 0] when given [1, 2, 3]", () => {
+		const result = fix23([1, 2, 3]);
+		assert.deepEqual(result, [1, 2, 0]);
+	});
+	it("Should return [2, 0, 5] when given [2, 3, 5]", () => {
+		const result = fix23([2, 3, 5]);
+		assert.deepEqual(result, [2, 0, 5]);
+	});
+	it("Should return [1, 2, 1] when given [1, 2, 1]", () => {
+		const result = fix23([1, 2, 1]);
+		assert.deepEqual(result, [1, 2, 1]);
 	});
 });
