34dd3dcba3ebc77e6b6292789d293549072175ec
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-10-11 08:59:26 +0200

    add start1

--- Array-1/start1/src/index.ts
+++ Array-1/start1/src/index.ts
@@ -1,3 +1,10 @@
-export function commonEnd(input: number): number {
-	return
+export function start1(inputA: number[], inputB: number[]): number {
+	if(inputA[0] == 1 && inputB[0] == 1){
+		return 2
+	}
+	if(inputA[0] == 1 || inputB[0] == 1){
+		return 1
+	}
+
+	return 0
 }
--- Array-1/start1/test/index.test.ts
+++ Array-1/start1/test/index.test.ts
@@ -1,13 +1,27 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { start1 } from "../src/index.js";
 
 /*
 
+Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], []) → 1
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("start1", () => {
+	it("Should return 2 when given [1, 2, 3], [1, 3]", () => {
+		const result = start1([1, 2, 3], [1, 3]);
+		assert.equal(result, 2);
+	});
+	it("Should return 1 when given [7, 2, 3], [1]", () => {
+		const result = start1([7, 2, 3], [1]);
+		assert.equal(result, 1);
+	});
+	it("Should return 1 when given [1, 2], []", () => {
+		const result = start1([1, 2], []);
+		assert.equal(result, 1);
 	});
 });
