538574f12380635df05a7c6f14c2611d79015fbf
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-10-11 08:58:49 +0200

    add makeEnds

--- Array-1/makeEnds/src/index.ts
+++ Array-1/makeEnds/src/index.ts
@@ -1,3 +1,5 @@
-export function commonEnd(input: number): number {
-	return
+export function makeEnds(input: number[]): number[] {
+	const first = input[0]
+	const last = input[input.length-1]
+	return [first,last]
 }
--- Array-1/makeEnds/test/index.test.ts
+++ Array-1/makeEnds/test/index.test.ts
@@ -1,13 +1,25 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { makeEnds } from "../src/index.js";
 
 /*
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
 
+makeEnds([1, 2, 3]) → [1, 3]
+makeEnds([1, 2, 3, 4]) → [1, 4]
+makeEnds([7, 4, 6, 2]) → [7, 2]
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("makeEnds", () => {
+	it("Should return [1, 3] when given [1,2,3]", () => {
+		const result = makeEnds([1,2,3]);
+		assert.deepEqual(result, [1,3]);
+	});
+	it("Should return [1, 4] when given [1, 2, 3, 4]", () => {
+		const result = makeEnds([1, 2, 3, 4]);
+		assert.deepEqual(result, [1, 4]);
+	});
+	it("Should return [7,2] when given [7, 4, 6, 2]", () => {
+		const result = makeEnds([7, 4, 6, 2]);
+		assert.deepEqual(result, [7, 2]);
 	});
 });
