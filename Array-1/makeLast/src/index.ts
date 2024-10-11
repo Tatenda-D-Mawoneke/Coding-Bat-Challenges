878fc926a512cead4714a34e53d13e27d9c2a141
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-10-11 08:58:59 +0200

    add makeLast

--- Array-1/makeLast/src/index.ts
+++ Array-1/makeLast/src/index.ts
@@ -1,3 +1,7 @@
-export function commonEnd(input: number): number {
-	return
+export function makeLast(input: number[]): number[] {
+	const last = input[input.length - 1];
+	let newArray = new Array(input.length * 2).fill(0);
+	newArray[newArray.length - 1] = last;
+	
+	return newArray;
 }
--- Array-1/makeLast/test/index.test.ts
+++ Array-1/makeLast/test/index.test.ts
@@ -1,13 +1,27 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { makeLast } from "../src/index.js";
 
 /*
 
+Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more. Note: by default, a new int array contains all 0's.
+
+
+makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+makeLast([1, 2]) → [0, 0, 0, 2]
+makeLast([3]) → [0, 3]
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("makeLast", () => {
+	it("Should return [0, 0, 0, 0, 0, 6] when given [4, 5, 6]", () => {
+		const result = makeLast([4, 5, 6]);
+		assert.deepEqual(result, [0, 0, 0, 0, 0, 6]);
+	});
+	it("Should return [0, 0, 0, 2] when given [1, 2]", () => {
+		const result = makeLast([1, 2]);
+		assert.deepEqual(result, [0, 0, 0, 2]);
+	});
+	it("Should return [0, 3] when given [3]", () => {
+		const result = makeLast([3]);
+		assert.deepEqual(result, [0, 3]);
 	});
 });
