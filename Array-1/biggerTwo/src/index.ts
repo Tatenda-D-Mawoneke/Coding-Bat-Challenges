d4c1407ea5fc9c07649ce08c757b9f083de1501d
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-10-11 08:57:50 +0200

    add biggerTwo

--- Array-1/biggerTwo/src/index.ts
+++ Array-1/biggerTwo/src/index.ts
@@ -1,3 +1,6 @@
-export function commonEnd(input: number): number {
-	return
+export function biggerTwo(inputA: number[], inputB: number[]): number[] {
+	const sumA = inputA.reduce((accumulator, currentValue) => accumulator + currentValue)
+	const sumB = inputB.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+	return sumB > sumA ? inputB : inputA
 }
--- Array-1/biggerTwo/test/index.test.ts
+++ Array-1/biggerTwo/test/index.test.ts
@@ -1,13 +1,27 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { biggerTwo } from "../src/index.js";
 
 /*
 
+Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+
+
+biggerTwo([1, 2], [3, 4]) → [3, 4]
+biggerTwo([3, 4], [1, 2]) → [3, 4]
+biggerTwo([1, 1], [1, 2]) → [1, 2]
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("biggerTwo", () => {
+	it("Should return [3, 4] when given [1, 2], [3, 4]", () => {
+		const result = biggerTwo([1, 2], [3, 4]);
+		assert.deepEqual(result, [3, 4])
+	});
+	it("Should return [3, 4] when given [3, 4], [1, 2]", () => {
+		const result = biggerTwo([3, 4], [1, 2]);
+		assert.deepEqual(result, [3, 4])
+	});
+	it("Should return [1, 2] when given [1, 1], [1, 2]", () => {
+		const result = biggerTwo([1, 1], [1, 2]);
+		assert.deepEqual(result, [1, 2])
 	});
 });
