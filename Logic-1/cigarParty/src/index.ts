b3d32a7ab454d0872f69e94b9346e3ec76ce388d
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-12-04 09:05:36 +0200

    add cigarParty

--- Logic-1/cigarParty/src/index.ts
+++ Logic-1/cigarParty/src/index.ts
@@ -1,3 +1,11 @@
-export function commonEnd(input: number): number {
-	return
+export function cigarParty(input: number, isWeekend: boolean): boolean {
+	if (isWeekend) {
+		if (input >= 40) {
+			return true;
+		}
+		return false;
+	} else if (input >= 40 && input <= 60) {
+		return true;
+	}
+	return false;
 }
--- Logic-1/cigarParty/test/index.test.ts
+++ Logic-1/cigarParty/test/index.test.ts
@@ -1,13 +1,26 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { cigarParty } from "../src/index.js";
 
 /*
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
 
+
+cigarParty(30, false) → false
+cigarParty(50, false) → true
+cigarParty(70, true) → true
 */
 
 describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+	it("Should return true when given '30, false'", () => {
+		const result = cigarParty(30, false);
+		assert.equal(result, false);
+	});
+	it("Should return true when given '50, false'", () => {
+		const result = cigarParty(50, false);
+		assert.equal(result, true);
+	});
+	it("Should return true when given '70, true'", () => {
+		const result = cigarParty(70, true);
+		assert.equal(result, true);
 	});
 });
