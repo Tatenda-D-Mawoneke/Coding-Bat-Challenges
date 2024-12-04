e10cd1f65b3b2024e2fe46d98bd7fbd00fcbfb1c
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-12-04 09:08:40 +0200

    add countHi

--- String-2/countHi/src/index.ts
+++ String-2/countHi/src/index.ts
@@ -1,3 +1,13 @@
-export function commonEnd(input: number): number {
-	return
+export function countHi(input: string): number {
+	let count = 0;
+
+	for (let i = 0; i < input.length; i++) {
+		const word1 = input[i];
+		const word2 = input[i + 1];
+		const word3 = word1 + word2;
+		if (word3 == "hi") {
+			count++;
+		}
+	}
+	return count;
 }
