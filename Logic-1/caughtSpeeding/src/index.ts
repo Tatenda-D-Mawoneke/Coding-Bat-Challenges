64fe229874adc098d4f57d1e0ad74d0d6ff0786e
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-12-04 09:05:21 +0200

    add caughtSpeeding

--- Logic-1/caughtSpeeding/src/index.ts
+++ Logic-1/caughtSpeeding/src/index.ts
@@ -1,3 +1,19 @@
-export function commonEnd(input: number): number {
-	return
+export function caughtSpeeding(speed: number, isBirthday: boolean): number {
+	let speedLowerLimit = 61;
+	let speedUpperLimit = 80;
+	if (isBirthday) {
+		speedLowerLimit += 5;
+		speedUpperLimit += 5;
+	}
+
+	return speedTest(speed, speedLowerLimit, speedUpperLimit);
+}
+
+function speedTest(speed: number, lowerLimit: number, upperLimit: number): number {
+	if (speed >= upperLimit) {
+		return 2;
+	} else if (speed >= lowerLimit && speed <= upperLimit) {
+		return 1;
+	}
+	return 0;
 }
--- Logic-1/caughtSpeeding/test/index.test.ts
+++ Logic-1/caughtSpeeding/test/index.test.ts
@@ -1,13 +1,26 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { caughtSpeeding } from "../src/index.js";
 
 /*
+You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
 
+
+caughtSpeeding(60, false) → 0
+caughtSpeeding(65, false) → 1
+caughtSpeeding(65, true) → 0
 */
 
 describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+	it("Should return 0 when given (60, false)", () => {
+		const result = caughtSpeeding(60, false);
+		assert.equal(result, 0);
+	});
+	it("Should return 1 when given (65, false)", () => {
+		const result = caughtSpeeding(65, false);
+		assert.equal(result, 1);
+	});
+	it("Should return 0 when given (65, true)", () => {
+		const result = caughtSpeeding(65, true);
+		assert.equal(result, 0);
 	});
 });
