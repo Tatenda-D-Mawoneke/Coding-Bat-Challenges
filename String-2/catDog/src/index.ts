560c9469f977c9d7dc73866612ca8fbe4b7c2c3b
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-12-04 09:08:12 +0200

    add catDog

--- String-2/catDog/src/index.ts
+++ String-2/catDog/src/index.ts
@@ -1,3 +1,15 @@
-export function commonEnd(input: number): number {
-	return
+export function catDog(input: string): boolean {
+	let catCount = 0
+	let dogCount = 0
+	for(let i = 0; i < input.length; i++){
+		let check = input[i]+input[i+1]+input[i+2]
+		if(check == "dog"){
+			dogCount++
+		}
+		if(check == "cat"){
+			catCount++
+		}
+	}
+
+	return catCount == dogCount
 }
--- String-2/catDog/test/index.test.ts
+++ String-2/catDog/test/index.test.ts
@@ -1,13 +1,25 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { catDog } from "../src/index.js";
 
 /*
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
 
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("catDog", () => {
+	it("Should return true when given catdog", () => {
+		const result = catDog("catdog");
+		assert.equal(result, true);
+	});
+	it("Should return false when given catcat", () => {
+		const result = catDog("catcat");
+		assert.equal(result, false);
+	});
+	it("Should return true when given 1cat1cadodog", () => {
+		const result = catDog("1cat1cadodog");
+		assert.equal(result, true);
 	});
 });
