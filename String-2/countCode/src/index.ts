e00a9bb44b6041c43a9b36d75c22241ff49ef984
Tatenda Mawoneke <tdmawoneke@gmail.com>
2024-12-04 09:08:26 +0200

    add countCode

--- String-2/countCode/src/index.ts
+++ String-2/countCode/src/index.ts
@@ -1,3 +1,11 @@
-export function commonEnd(input: number): number {
-	return
+export function countCode(input: string): number {
+	let result = 0;
+	for (let i = 0; i < input.length; i++) {
+		let word1 = input[i] + input[i + 1] 
+		let word2 = input[i+3]
+		if (word1 == "co" && word2 == "e") {
+			result++;
+		}
+	}
+	return result;
 }
--- String-2/countCode/test/index.test.ts
+++ String-2/countCode/test/index.test.ts
@@ -1,13 +1,25 @@
 import { assert } from "chai";
-import { commonEnd } from "../src/index.js";
+import { countCode } from "../src/index.js";
 
 /*
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
 
+countCode("aaacodebbb") → 1
+countCode("codexxcode") → 2
+countCode("cozexxcope") → 2
 */
 
-describe("Template", () => {
-	it("Should ", () => {
-		const result = commonEnd("A");
-		assert.equal(result, "A");
+describe("countCode", () => {
+	it("Should return 1 when given 'aaacodebbb'", () => {
+		const result = countCode("aaacodebbb");
+		assert.equal(result, 1);
+	});
+	it("Should return 2 when given 'codexxcode'", () => {
+		const result = countCode("codexxcode");
+		assert.equal(result, 2);
+	});
+	it("Should return 2 when given 'cozexxcope'", () => {
+		const result = countCode("cozexxcope");
+		assert.equal(result, 2);
 	});
 });
